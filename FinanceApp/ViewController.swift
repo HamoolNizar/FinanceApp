603735cfe9e30cb84928ca231bacf247a78e1d43
Hamool NIzar <Hamool.2015343@iit.ac.lk>
2020-02-15 22:39:36 +0530

    Initial Commit

---
+++ FinanceApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FinanceApp
+//
+//  Created by Hamool Nizar on 2/15/20.
+//  Copyright © 2020 Hamool Nizar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
